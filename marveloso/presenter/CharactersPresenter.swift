c0320cbc5fd8f82dc1255ae9e38db89dbfc75544
Javi Mollá <javier.molla.moreno@gmail.com>
2021-02-27 17:28:49 +0100

    Crear la pantalla del listado de caracteres

---
+++ marveloso/presenter/CharactersPresenter.swift
@@ -0,0 +1,34 @@
+//
+//  CharactersPresenter.swift
+//  marveloso
+//
+//  Created by liver6 on 25/02/2021.
+//
+
+import Foundation
+
+protocol CharactersViewPresenter: class {
+    func loadCharacters(_ first: Int)
+}
+
+class CharactersPresenter: CharactersViewPresenter {
+    weak var view: CharactersView?
+    
+    let marvelService = MarvelService()
+    
+    required init(view: CharactersView) {
+        self.view = view
+    }
+    
+    func loadCharacters(_ first: Int) {
+        self.marvelService.getCharactersSimple(first) { (characters: [CharacterSimple]?,
+                                                         totalCharacters: Int?,
+                                                         error: MarvelService.MarvelServiceError?) in
+            if (error == nil) {
+                self.view?.onCharactersRetrieved(characters!, totalCharacters!)
+            } else {
+                self.view?.onError(error!)
+            }
+        }
+    }
+}
