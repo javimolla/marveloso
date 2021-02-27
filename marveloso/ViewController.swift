1c9d1650200864d8fe7c80ed147b9d3e5b01dbc8
Javi Mollá <javier.molla.moreno@gmail.com>
2021-02-27 08:53:57 +0100

    Crear el proyecto vacío con XCode

---
+++ marveloso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marveloso
+//
+//  Created by liver6 on 27/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
