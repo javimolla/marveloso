9b5aa88ffd15d8776915c5a0fcce0f147a9c3994
Javi Mollá <javier.molla.moreno@gmail.com>
2021-02-27 13:18:43 +0100

    Crear los tests para probar el servicio de Marvel

---
+++ marvelosoTests/marvelServiceTests.swift
@@ -0,0 +1,56 @@
+//
+//  marvelServiceTests.swift
+//  marvelosoTests
+//
+//  Created by javimolla on 27/02/2021.
+//
+
+import XCTest
+@testable import marveloso
+
+class marvelServiceTests: XCTestCase {
+    override func setUpWithError() throws {
+        SwaggerClientAPI.publicKey = environmentVariables.marvelPublicKey
+        SwaggerClientAPI.privateKey = environmentVariables.marvelPrivateKey
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testGetSomeCharacters() throws {
+        let expectation = XCTestExpectation(description: "Obtener los datos de algunos personajes")
+        
+        MarvelService().getCharactersSimple(0) { (characters: [CharacterSimple]?,
+                                                  totalCharacters: Int?,
+                                                  error: MarvelService.MarvelServiceError?) in
+            XCTAssertNil(error)
+            XCTAssertGreaterThan(totalCharacters!, characters!.count)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testGetOneCharacter() throws {
+        let expectation = XCTestExpectation(description: "Obtener los datos de un personaje")
+        
+        let id = 1011334
+        MarvelService().getCharacterDetail(id) { (character: CharacterDetail?,
+                                                  error: MarvelService.MarvelServiceError?) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(character)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
